f822837dd6e36315c624fa21674e3008568ebea8
David A. Mellis <dam@mellis.org>
2015-02-04 17:10:23 -0500

    Update pin numbers to match board.

--- Arduino/WebClient/WebClient.ino
+++ Arduino/WebClient/WebClient.ino
@@ -26,16 +26,15 @@ SmartConfig is still beta and kind of works but is not fully vetted!
 It might not work on all networks!
 */
 #include <Adafruit_CC3000.h>
-#include <ccspi.h>
 #include <SPI.h>
 #include <string.h>
 #include "utility/debug.h"
 
 // These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   2  // MUST be an interrupt pin!
+#define ADAFRUIT_CC3000_IRQ   22  // MUST be an interrupt pin!
 // These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  20
-#define ADAFRUIT_CC3000_CS    19
+#define ADAFRUIT_CC3000_VBAT  23
+#define ADAFRUIT_CC3000_CS    21
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
