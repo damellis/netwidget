c52f985c9191a788eb23c9bf385640a40de0bc79
David A. Mellis <dam@mellis.org>
2015-03-14 16:23:37 -0400

    Update to current PCB.

--- Arduino/TouchGlow/TouchGlow.ino
+++ Arduino/TouchGlow/TouchGlow.ino
@@ -1,6 +1,6 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor   cs = CapacitiveSensor(3,2);     // load, sense
+CapacitiveSensor   cs = CapacitiveSensor(26,27);     // load, sense
 int R = 5, G = 6, B = 10;
 int fade = 0;
 
@@ -23,9 +23,9 @@ void loop()
     Serial.print("\t");
     Serial.println(fade);
     
-    analogWrite(R, 255 - constrain(fade, 0, 255));
-    analogWrite(G, 255 - constrain(fade, 0, 255));
-    analogWrite(B, 255 - constrain(fade, 0, 255));
+    analogWrite(R, constrain(fade, 0, 255));
+    analogWrite(G, constrain(fade, 0, 255));
+    analogWrite(B, constrain(fade, 0, 255));
 
     delay(10);
 }
